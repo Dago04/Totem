9191b69416741fe1b6a05af57103a3e36ed05515
Dagoberto Salas <DagoSalas1999@gmail.com>
2024-05-04 12:02:13 -0600

    Adding data benefits

---
+++ src/totem/data/benefits.js
@@ -0,0 +1,21 @@
+import { FaShoppingBasket, FaRegEdit, FaShippingFast } from "react-icons/fa";
+export const benefits = [
+    {
+        id: 1,
+        title: 'Variedad de productos',
+        description: 'Explora nuestra amplia selección de camisas con una variedad de diseños únicos y exclusivos. Desde estilos clásicos hasta tendencias de moda, tenemos algo para todos los gustos y ocasiones.',
+        icon: FaShoppingBasket,
+    },
+    {
+        id: 2,
+        title: 'Diseños Personalizados',
+        description: 'Haz una declaración de estilo con nuestras camisas personalizadas. Trabaja con nuestro equipo de diseño para crear una prenda que refleje tu personalidad y gustos únicos, desde estampados exclusivos hasta detalles personalizados.',
+        icon: FaRegEdit
+    },
+    {
+        id: 3,
+        title: 'Envios a todo el país',
+        description: 'No importa dónde te encuentres, te llevamos la moda directamente a tu puerta. Con envíos a todo el país, puedes disfrutar de la comodidad de comprar tus camisas favoritas desde la comodidad de tu hogar, sin importar tu ubicación.',
+        icon: FaShippingFast
+    },
+]
