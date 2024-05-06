9ff5fd08f2c9830585bb8257af085840098dcd69
Dagoberto Salas <DagoSalas1999@gmail.com>
2024-05-05 22:51:59 -0600

    changing data names to english

--- src/totem/components/AnimeShirt.jsx
+++ src/totem/components/AnimeShirt.jsx
@@ -4,9 +4,9 @@ export const AnimeShirt = ({ id, size, price, category, anime, image }) => {
             <div className=" rounded overflow-hidden">
                 <img alt={anime} className="" src={image} />
             </div>
-            <div className="mt-4 p-4">
-                <h3 className="mt-1">CATEGORY - {category}</h3>
-                <h2 className="mt-1">ANIME - {anime}</h2>
+            <div className="mt-2 p-4">
+                <h3 className="mt-1">Category - {category}</h3>
+                <h2 className="mt-1">Anime - {anime}</h2>
                 <p className="mt-1">Prize - {price}</p>
                 <p className="mt-1">Size - {size}</p>
             </div>
