436c8b423986c7580f6f81bcfda8a5053203e820
Dagoberto Salas <DagoSalas1999@gmail.com>
2024-05-05 21:35:51 -0600

    camisasAnime.js

---
+++ src/totem/data/camisasAnime.js
@@ -0,0 +1,575 @@
+export const camisasAnime = [
+    {
+        id: 1,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Other',
+        image: '/Camisas/anime-1.jpeg'
+
+    },
+    {
+        id: 2,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Other',
+        image: '/Camisas/anime-2.jpeg'
+    },
+    {
+        id: 3,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Other',
+        image: '/Camisas/anime-3.jpeg'
+    },
+    {
+        id: 4,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Other',
+        image: '/Camisas/anime-4.jpeg'
+    },
+    {
+        id: 5,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Other',
+        image: '/Camisas/anime-5.jpeg'
+    },
+    {
+        id: 6,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Other',
+        image: '/Camisas/anime-6.jpeg'
+    },
+    {
+        id: 7,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Other',
+        image: '/Camisas/anime-7.jpeg'
+    },
+    {
+        id: 8,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Other',
+        image: '/Camisas/anime-8.jpeg'
+    },
+    {
+        id: 9,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Other',
+        image: '/Camisas/anime-9.jpeg'
+    },
+    {
+        id: 10,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Other',
+        image: '/Camisas/anime-10.jpeg'
+    },
+    {
+        id: 11,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Bleach',
+        image: '/Camisas/anime-bleach-1.jpeg'
+    },
+    {
+        id: 12,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Bleach',
+        image: '/Camisas/anime-bleach-2.jpeg'
+    },
+    {
+        id: 13,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Blue Lock',
+        image: '/Camisas/anime-bluelock-1.jpeg'
+    },
+    {
+        id: 14,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Blue Lock',
+        image: '/Camisas/anime-bluelock-2.jpeg'
+    },
+    {
+        id: 15,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Dead Note',
+        image: '/Camisas/anime-deadnote-1.jpeg'
+    },
+    {
+        id: 16,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Dead Note',
+        image: '/Camisas/anime-deadnote-2.jpeg'
+    },
+    {
+        id: 17,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Dragon Ball',
+        image: '/Camisas/anime-dragonball-1.jpeg'
+    },
+    {
+        id: 18,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Dragon Ball',
+        image: '/Camisas/anime-dragonball-2.jpeg'
+    },
+    {
+        id: 19,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Dragon Ball',
+        image: '/Camisas/anime-dragonball-3.jpeg'
+    },
+    {
+        id: 20,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Dragon Ball',
+        image: '/Camisas/anime-dragonball-4.jpeg'
+    },
+    {
+        id: 21,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Haikyu',
+        image: '/Camisas/anime-haikyu-1.jpeg'
+    },
+    {
+        id: 22,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Haikyu',
+        image: '/Camisas/anime-haikyu-2.jpeg'
+    },
+    {
+        id: 23,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Haikyu',
+        image: '/Camisas/anime-haikyu-3.jpeg'
+    },
+    {
+        id: 24,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Haikyu',
+        image: '/Camisas/anime-haikyu-4.jpeg'
+    },
+    {
+        id: 25,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Haikyu',
+        image: '/Camisas/anime-haikyu-5.jpeg'
+    },
+    {
+        id: 26,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Hunter x Hunter',
+        image: '/Camisas/anime-hunterxhunter-1.jpeg'
+    },
+    {
+        id: 27,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Jujustu Kaisen',
+        image: '/Camisas/anime-jujutsu-1.jpeg'
+    },
+    {
+        id: 28,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Jujustu Kaisen',
+        image: '/Camisas/anime-jujutsu-2.jpeg'
+    },
+    {
+        id: 29,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Jujustu Kaisen',
+        image: '/Camisas/anime-jujutsu-3.jpeg'
+    },
+    {
+        id: 30,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Jujustu Kaisen',
+        image: '/Camisas/anime-jujutsu-4.jpeg'
+    },
+    {
+        id: 31,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Jujustu Kaisen',
+        image: '/Camisas/anime-jujutsu-5.jpeg'
+    },
+    {
+        id: 32,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Jujustu Kaisen',
+        image: '/Camisas/anime-jujutsu-6.jpeg'
+    },
+    {
+        id: 33,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Kimetsu no Yaiba',
+        image: '/Camisas/anime-kimetsu-1.jpeg'
+    },
+    {
+        id: 34,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Kimetsu no Yaiba',
+        image: '/Camisas/anime-kimetsu-2.jpeg'
+    },
+    {
+        id: 35,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Kimetsu no Yaiba',
+        image: '/Camisas/anime-kimetsu-3.jpeg'
+    },
+    {
+        id: 36,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Kimetsu no Yaiba',
+        image: '/Camisas/anime-kimetsu-4.jpeg'
+    },
+    {
+        id: 37,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Kimetsu no Yaiba',
+        image: '/Camisas/anime-kimetsu-5.jpeg'
+    },
+    {
+        id: 38,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Kimetsu no Yaiba',
+        image: '/Camisas/anime-kimetsu-6.jpeg'
+    },
+    {
+        id: 39,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Kimetsu no Yaiba',
+        image: '/Camisas/anime-kimetsu-7.jpeg'
+    },
+    {
+        id: 40,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Kimetsu no Yaiba',
+        image: '/Camisas/anime-kimetsu-8.jpeg'
+    },
+    {
+        id: 41,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Kimetsu no Yaiba',
+        image: '/Camisas/anime-kimetsu-9.jpeg'
+    },
+    {
+        id: 42,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Kimetsu no Yaiba',
+        image: '/Camisas/anime-kimetsu-10.jpeg'
+    },
+    {
+        id: 43,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Kimetsu no Yaiba',
+        image: '/Camisas/anime-kimetsu-11.jpeg'
+    },
+    {
+        id: 44,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'My Hero Academia',
+        image: '/Camisas/anime-myheroacademia-1.jpeg'
+    },
+    {
+        id: 43,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'My Hero Academia',
+        image: '/Camisas/anime-myheroacademia-2.jpeg'
+    },
+    {
+        id: 44,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'My Hero Academia',
+        image: '/Camisas/anime-myheroacademia-3.jpeg'
+    },
+    {
+        id: 45,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'My Hero Academia',
+        image: '/Camisas/anime-myheroacademia-4.jpeg'
+    },
+    {
+        id: 46,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'My Hero Academia',
+        image: '/Camisas/anime-myheroacademia-5.jpeg'
+    },
+    {
+        id: 47,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'My Hero Academia',
+        image: '/Camisas/anime-myheroacademia-6.jpeg'
+    },
+    {
+        id: 48,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'My Hero Academia',
+        image: '/Camisas/anime-myheroacademia-7.jpeg'
+    },
+    {
+        id: 49,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'My Hero Academia',
+        image: '/Camisas/anime-myheroacademia-8.jpeg'
+    },
+    {
+        id: 50,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'My Hero Academia',
+        image: '/Camisas/anime-myheroacademia-9.jpeg'
+    },
+    {
+        id: 51,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'My Hero Academia',
+        image: '/Camisas/anime-myheroacademia-10.jpeg'
+    },
+    {
+        id: 52,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'My Hero Academia',
+        image: '/Camisas/anime-myheroacademia-11.jpeg'
+    },
+    {
+        id: 53,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'My Hero Academia',
+        image: '/Camisas/anime-myheroacademia-12.jpeg'
+    },
+    {
+        id: 54,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'My Hero Academia',
+        image: '/Camisas/anime-myheroacademia-13.jpeg'
+    },
+    {
+        id: 55,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'My Hero Academia',
+        image: '/Camisas/anime-myheroacademia-14.jpeg'
+    },
+    {
+        id: 56,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'My Hero Academia',
+        image: '/Camisas/anime-myheroacademia-15.jpeg'
+    },
+    {
+        id: 57,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Naruto',
+        image: '/Camisas/anime-naruto-1.jpeg'
+    },
+    {
+        id: 58,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Naruto',
+        image: '/Camisas/anime-naruto-2.jpeg'
+    },
+    {
+        id: 59,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'One Piece',
+        image: '/Camisas/anime-onepiece-1.jpeg'
+    },
+    {
+        id: 60,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'One Piece',
+        image: '/Camisas/anime-onepiece-2.jpeg'
+    },
+    {
+        id: 61,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'One Punch Man',
+        image: '/Camisas/anime-onepunch-1.jpeg'
+    },
+    {
+        id: 62,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Pokemon',
+        image: '/Camisas/anime-pokemon-1.jpeg'
+    },
+    {
+        id: 63,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Pokemon',
+
+        image: '/Camisas/anime-pokemon-2.jpeg'
+    },
+    {
+        id: 64,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Saint Seiya',
+        image: '/Camisas/anime-saintseiya-1.jpeg'
+    },
+    {
+        id: 65,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Shingeki no Kyojin',
+        image: '/Camisas/anime-shingeki-1.jpeg'
+    },
+    {
+        id: 66,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Totoro',
+        image: '/Camisas/anime-totoro-1.jpeg'
+    },
+    {
+        id: 67,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Totoro',
+
+        image: '/Camisas/anime-totoro-2.jpeg'
+    },
+    {
+        id: 68,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Totoro',
+
+        image: '/Camisas/anime-totoro-3.jpeg'
+    },
+    {
+        id: 69,
+        size: 'S, M, L, XL, 2XL, 3XL',
+        price: '$20',
+        category: 'Anime',
+        anime: 'Totoro',
+
+        image: '/Camisas/anime-totoro-4.jpeg'
+    },
+]
