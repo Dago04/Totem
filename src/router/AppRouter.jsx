727c1f94e1a836e1cc76a2af9138f953e9041746
Diegocarque12 <diegocarque1213@gmail.com>
2024-05-07 07:17:36 -0600

    add a bg color

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import { TotemRoutes } from '../totem';
 export const AppRouter = () => {
-    return (
-        <>
-            <Routes>
-                <Route path='*' element={<TotemRoutes />} />
-            </Routes >
-        </>
-    )
-}
+  return (
+    <div className='bg-slate-200'>
+      <Routes>
+        <Route path='*' element={<TotemRoutes />} />
+      </Routes>
+    </div>
+  );
+};
