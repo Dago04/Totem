6bebe5149e612672db48297a8041c4add4451052
Diegocarque12 <diegocarque1213@gmail.com>
2024-05-07 07:18:15 -0600

    remove and add categories file data js

---
+++ src/totem/data/categories.js
@@ -0,0 +1,111 @@
+export const categorias = [
+  {
+    id: 1,
+    categoria: 'Anime',
+    urls: [
+      {
+        id: 1,
+        name: 'tanjiro',
+        url: '/categories/anime/tanjiro.jpeg',
+      },
+      {
+        id: 2,
+        name: 'haikyuu',
+        url: '/categories/anime/haikyuu.jpeg',
+      },
+      {
+        id: 3,
+        name: 'goku',
+        url: '/categories/anime/goku.png',
+      },
+      {
+        id: 4,
+        name: 'naruto',
+        url: '/categories/anime/naruto.jpg',
+      },
+    ],
+  },
+  {
+    id: 2,
+    categoria: 'Music',
+    urls: [
+      {
+        id: 1,
+        name: 'Iron Maiden',
+        url: '/categories/music/iron-maiden.jpg',
+      },
+      {
+        id: 2,
+        name: 'Metallica',
+        url: '/categories/music/metallica.webp',
+      },
+      {
+        id: 3,
+        name: 'System of a Down',
+        url: '/categories/music/system-of-a-down.webp',
+      },
+    ],
+  },
+  {
+    id: 3,
+    categoria: 'Games',
+    urls: [
+      {
+        id: 1,
+        name: 'Miss Fortune LoL',
+        url: '/categories/games/miss-fortune.jpeg',
+      },
+      {
+        id: 2,
+        name: 'blitzcrank LoL',
+        url: '/categories/games/blitz.jpeg',
+      },
+      {
+        id: 3,
+        name: 'Bomberman',
+        url: '/categories/games/bomberman.jpeg',
+      },
+      {
+        id: 4,
+        name: 'lux LoL',
+        url: '/categories/games/lux.jpeg',
+      },
+      {
+        id: 5,
+        name: 'Legend Of Zelda',
+        url: '/categories/games/majoras-mask.jpeg',
+      },
+      {
+        id: 6,
+        name: 'Snake Metal Gear Solid',
+        url: '/categories/games/snake.jpeg',
+      },
+    ],
+  },
+  {
+    id: 4,
+    categoria: 'Movies',
+    urls: [
+      {
+        id: 1,
+        name: 'batman',
+        url: '/categories/movies/batman.jpeg',
+      },
+      {
+        id: 2,
+        name: 'joker',
+        url: '/categories/movies/joker.jpeg',
+      },
+      {
+        id: 3,
+        name: 'Star Wars',
+        url: '/categories/movies/star-wars.jpeg',
+      },
+      {
+        id: 4,
+        name: 'superman',
+        url: '/categories/movies/superman.jpeg',
+      },
+    ],
+  },
+];
