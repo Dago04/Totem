e1146c6614531aafce42f8cb30a67ee48517f4b8
Diegocarque12 <diegocarque1213@gmail.com>
2024-05-07 22:51:19 -0600

    reduce time and make responsive

--- src/totem/components/CategoryCard.jsx
+++ src/totem/components/CategoryCard.jsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
 export const CategoryCard = ({ id, categoria, urls }) => {
   const [mouseIn, setMouseIn] = useState(false);
@@ -18,43 +18,45 @@ export const CategoryCard = ({ id, categoria, urls }) => {
     const interval = setInterval(() => {
       setCurrentImage(urls[currentIndex]);
       setCurrentIndex((currentIndex + 1) % urls.length);
-    }, 10000);
+    }, 5000);
 
     return () => clearInterval(interval);
   }, [currentIndex, urls]);
 
   return (
     <Link to={`/shirts/${categoria.toLowerCase()}`}>
-      < article
+      <article
         key={id}
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
-        className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl pb-8 w-[18rem] max-w-full mx-auto mt-4 transition ease-in-out hover:-translate-y-1 hover:scale-105 duration-300 h-[30rem] shadow-2xl pl-8"
+        className='relative isolate flex flex-col justify-end overflow-hidden rounded-2xl pb-8 w-[18rem] max-w-full mx-auto mt-4 transition ease-in-out hover:-translate-y-1 hover:scale-105 duration-300 h-[30rem] shadow-2xl pl-8'
       >
         <img
           src={currentImage.url}
-          alt="Category Image"
-          className="absolute inset-0 h-full w-full object-cover"
+          alt='Category Image'
+          className='absolute inset-0 h-full w-full object-cover'
         />
         <div
-          className={`absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40 ease-in-out transition-all duration-300 ${mouseIn ? "opacity-100" : "opacity-0"
-            }`}
+          className={`absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40 ease-in-out transition-all duration-300 ${
+            mouseIn ? 'lg:opacity-100' : 'lg:opacity-0'
+          }`}
         ></div>
         <h3
-          className={`z-10 mt-3 text-3xl font-bold text-white transition-all duration-300 ${mouseIn ? "opacity-100" : "opacity-0"
-            }`}
+          className={`z-10 mt-3 text-3xl font-bold text-white transition-all duration-300 ${
+            mouseIn ? 'lg:opacity-100' : 'lg:opacity-0'
+          }`}
         >
           {categoria}
         </h3>
         <div
-          className={`z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-300 ${mouseIn ? "block delay-150" : "invisible delay-150"
-            }`}
+          className={`z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-300 ${
+            mouseIn ? 'lg:block lg:delay-150' : 'lg:invisible lg:delay-150'
+          }`}
         >
-          <span className="capitalize">{currentImage.name}</span>
+          <span className='capitalize'>{currentImage.name}</span>
         </div>
-      </article >
-    </Link >
-
+      </article>
+    </Link>
   );
 };
 
