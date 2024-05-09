28527186a2cda2a413e1ecbe9c15e36b31e892e2
Dagoberto Salas <DagoSalas1999@gmail.com>
2024-05-08 21:52:29 -0600

    hamburguer menu and hover effect to anime and music card

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,40 +1,50 @@
-import { Link, NavLink } from 'react-router-dom';
-import Logo from '/Logo.png';
+import { NavLink } from "react-router-dom";
+import Logo from "/Logo.png";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { useState } from 'react';
+import { useState } from "react";
 
 export const Navbar = () => {
-  const [openMenu, setOpenMenu] = useState(false)
+  const [openMenu, setOpenMenu] = useState(false);
   const handleOpenMenu = () => {
-    setOpenMenu(!openMenu)
-  }
+    setOpenMenu(!openMenu);
+  };
 
   return (
-    <header className='bg-slate-950/90 text-white/90 text-xl'>
-      <nav className=' flex flex-col items-center container mx-auto  py-3 gap-5 lg:flex-row '>
-        <img className='w-20 ' src={Logo} alt='Logo Totem' />
-        <Link className=' hover:opacity-80' to='/'>
-          Inicio
-        </Link>
-
-        <div className='flex justify-center w-full lg:justify-start'>
-          <NavLink
-            className={({ isActive }) =>
-              `hover:opacity-80 text-md font-bold ${isActive ? 'text-blue-600' : 'text-white/50'
-              } `
-            }
-            to='/categories'
-          >
-            Categorias
-          </NavLink>
+    <header className="bg-slate-950/90 text-white/90 text-xl">
+      <nav className="flex justify-between lg:justify-start items-center container mx-auto py-3 gap-5 lg:flex-row ">
+        <img className="w-20" src={Logo} alt="Logo Totem" />
+        <GiHamburgerMenu className="mr-5 lg:hidden" onClick={handleOpenMenu} />
+        <div className="hidden lg:flex gap-2 relative">
+          <NavItems />
         </div>
 
-        <GiHamburgerMenu className=' md:hidden' />
-
+        {openMenu && <NavItems />}
       </nav>
     </header>
   );
 };
 
-const
-
+const NavItems = () => {
+  return (
+    <div className="absolute lg:relative lg:top-0 lg:bg-transparent lg:flex lg:flex-row lg:text-xl bg-slate-950 top-24 p-4 w-full flex flex-col items-center text-3xl py-8 gap-3">
+      <NavLink
+        className={({ isActive }) =>
+          `hover:opacity-80  font-bold ${isActive ? "text-blue-500" : "text-white/50"
+          } `
+        }
+        to="home"
+      >
+        Inicio
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          `hover:opacity-80  font-bold ${isActive ? "text-blue-500" : "text-white/50"
+          } `
+        }
+        to="/categories"
+      >
+        Categorias
+      </NavLink>
+    </div>
+  );
+};
