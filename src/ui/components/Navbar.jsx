320d4dc4a9adec8cd794e8d249b176f7f2aa4e7e
Dagoberto Salas <DagoSalas1999@gmail.com>
2024-05-09 15:13:44 -0600

    Creating NavModal.jsx

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -17,11 +17,9 @@ export const Navbar = () => {
       <nav className="flex justify-between lg:justify-start items-center container mx-auto py-3 gap-5 lg:flex-row ">
         <img className="w-20" src={Logo} alt="Logo Totem" />
         <GiHamburgerMenu className="mr-5 lg:hidden" onClick={handleOpenMenu} />
-        <div className="hidden lg:flex gap-2 relative">
-          <NavItems />
-        </div>
 
-        {openMenu && <NavItems handleCloseMenu={handleCloseMenu} />}
+        <NavItems />
+        {openMenu && <NavModal handleCloseMenu={handleCloseMenu} />}
       </nav>
     </header>
   );
@@ -29,7 +27,7 @@ export const Navbar = () => {
 
 const NavItems = ({ handleCloseMenu }) => {
   return (
-    <div className="absolute z-10 lg:relative lg:top-0 lg:bg-transparent lg:flex lg:flex-row lg:text-xl bg-slate-950 top-24 left-0 p-4 w-full flex flex-col items-center text-3xl py-8 gap-3">
+    <div className="hidden lg:top-0 lg:bg-transparent lg:flex lg:flex-row lg:text-xl bg-slate-950 gap-3 lg:w-full">
       <NavLink
         className={({ isActive }) =>
           `hover:opacity-80  font-bold ${isActive ? "text-blue-500" : "text-white/50"
@@ -54,3 +52,31 @@ const NavItems = ({ handleCloseMenu }) => {
     </div >
   );
 };
+
+const NavModal = ({ handleCloseMenu }) => {
+  return (
+    <div className="absolute z-10  bg-slate-950 top-24 left-0 p-4 w-full flex flex-col items-center text-3xl py-8 gap-3">
+      <NavLink
+        className={({ isActive }) =>
+          `hover:opacity-80  font-bold ${isActive ? "text-blue-500" : "text-white/50"
+          } `
+        }
+        to="home"
+        onClick={handleCloseMenu}
+      >
+        Inicio
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          `hover:opacity-80  font-bold ${isActive ? "text-blue-500" : "text-white/50"
+          } `
+        }
+        to="/categories"
+        onClick={handleCloseMenu}
+
+      >
+        Categorias
+      </NavLink>
+    </div >
+  )
+}
