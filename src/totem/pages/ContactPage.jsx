c7c9f1ec6f921074c7992df02f36141084c9ac4e
Dagoberto Salas <DagoSalas1999@gmail.com>
2024-05-11 22:26:53 -0600

    Working on contact form

--- src/totem/pages/ContactPage.jsx
+++ src/totem/pages/ContactPage.jsx
@@ -2,8 +2,8 @@ import { MapEmbed } from "../components";
 import { ContactForm } from "../components/ContactForm";
 export const ContactPage = () => {
     return (
-        <main className="min-h-[78vh]">
-            <section className="flex flex-col px-3 justify-center items-start lg:flex-row lg:px-20 lg:py-20 lg:gap-8">
+        <main className="min-h-[78vh] container mx-auto">
+            <section className="flex flex-col px-3 justify-center items-start lg:flex-row lg:px-20 lg:py-40 lg:gap-8">
                 {/* <article className="w-full py-8 lg:max-w-[700px]">
                     <h1 className="text-center text-3xl font-extrabold leading-none tracking-tight text-gray-900 sm:text-4xl lg:text-5xl">
                         ¿Buscas la camisa perfecta? <br />
@@ -14,7 +14,7 @@ export const ContactPage = () => {
                 </article> */}
                 <ContactForm />
 
-                <article className="w-full mb-20 lg:max-w-[600px]">
+                <article className="w-full mb-12 lg:max-w-[600px]">
                     <MapEmbed />
                 </article>
             </section>
