627d547f539ca80667df4435c338e526a3ba9b73
Dagoberto Salas <DagoSalas1999@gmail.com>
2024-05-30 21:04:38 -0600

    Adding lfc data

--- src/totem/data/musicShirts.js
+++ src/totem/data/musicShirts.js
@@ -333,8 +333,382 @@ export const camisasMusica = [
         price: '$20',
         category: 'Music',
         group: 'Los Fabulosos Cadillacs',
-        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabulosos-cadillacs-01.jpg'
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-01.jpg'
     },
-
-
+    {
+        id: 42,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-02.jpg'
+    },
+    {
+        id: 43,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-03.jpg'
+    },
+    {
+        id: 44,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-04.jpg'
+    },
+    {
+        id: 45,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-05.jpg'
+    },
+    {
+        id: 46,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-06.jpg'
+    },
+    {
+        id: 47,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-07.jpg'
+    },
+    {
+        id: 48,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-08.jpg'
+    },
+    {
+        id: 49,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-09.jpg'
+    },
+    {
+        id: 50,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-10.jpg'
+    },
+    {
+        id: 51,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-11.jpg'
+    },
+    {
+        id: 52,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-12.jpg'
+    },
+    {
+        id: 53,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-13.jpg'
+    },
+    {
+        id: 54,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-14.jpg'
+    },
+    {
+        id: 55,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-15.jpg'
+    },
+    {
+        id: 56,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-16.jpg'
+    },
+    {
+        id: 57,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-17.jpg'
+    },
+    {
+        id: 58,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-18.jpg'
+    },
+    {
+        id: 59,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-19.jpg'
+    },
+    {
+        id: 60,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-20.jpg'
+    },
+    {
+        id: 61,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-21.jpg'
+    },
+    {
+        id: 62,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-22.jpg'
+    },
+    {
+        id: 63,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-23.jpg'
+    },
+    {
+        id: 64,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-24.jpg'
+    },
+    {
+        id: 65,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-25.jpg'
+    },
+    {
+        id: 66,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-26.jpg'
+    },
+    {
+        id: 67,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-27.jpg'
+    },
+    {
+        id: 68,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-28.jpg'
+    },
+    {
+        id: 69,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-29.jpg'
+    },
+    {
+        id: 70,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-30.jpg'
+    },
+    {
+        id: 71,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-31.jpg'
+    },
+    {
+        id: 72,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-32.jpg'
+    },
+    {
+        id: 73,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-33.jpg'
+    },
+    {
+        id: 74,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-34.jpg'
+    },
+    {
+        id: 75,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-35.jpg'
+    },
+    {
+        id: 76,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-36.jpg'
+    },
+    {
+        id: 77,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-37.jpg'
+    },
+    {
+        id: 78,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-38.jpg'
+    },
+    {
+        id: 79,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-39.jpg'
+    },
+    {
+        id: 80,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-40.jpg'
+    },
+    {
+        id: 81,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-41.jpg'
+    },
+    {
+        id: 82,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-42.jpg'
+    },
+    {
+        id: 83,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-43.jpg'
+    },
+    {
+        id: 84,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-44.jpg'
+    },
+    {
+        id: 85,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-45.jpg'
+    },
+    {
+        id: 86,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-46.jpg'
+    },
+    {
+        id: 87,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-47.jpg'
+    },
+    {
+        id: 88,
+        size: ['S', 'M', 'L', 'XL', '2XL', '3XL'],
+        price: '$20',
+        category: 'Music',
+        group: 'Los Fabulosos Cadillacs',
+        image: '/Camisas/Los-fabulosos-cadillacs/Mkp-los-fabuloso-cadillacs-48.jpg'
+    },
 ]
