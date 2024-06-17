ebd41039c2cc6d16673cb1ce0f30fba0b066f48b
Dagoberto Salas <DagoSalas1999@gmail.com>
2024-06-16 22:44:38 -0600

    Adding CheckingAuth component

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 import { TotemRoutes } from '../totem';
 import { AuthRoutes } from '../auth/routes/AuthRoutes';
+import { useSelector } from 'react-redux';
+import { CheckingAuth } from '../ui';
 export const AppRouter = () => {
+
+  const { status } = useSelector((state) => state.auth);
+
+  if (status === 'checking') {
+    return (
+      <CheckingAuth />
+    );
+  }
   return (
     <div className='bg-slate-200 h-full'>
       <Routes>
