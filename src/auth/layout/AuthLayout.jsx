28993a20f94c816fa0b29d17c97fbcbfb6397255
Dagoberto Salas <DagoSalas1999@gmail.com>
2024-06-17 21:51:56 -0600

    center login page

--- src/auth/layout/AuthLayout.jsx
+++ src/auth/layout/AuthLayout.jsx
@@ -2,9 +2,9 @@
 export const AuthLayout = ({ children }) => {
     return (
         <div className="text-white/90  ">
-            <div className="p-2 min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 ">
+            <div className="items-center p-2 min-h-screen flex flex-col justify-center  sm:justify-center  pt-6 sm:pt-0 ">
 
-                <div className="w-full max-w-md px-6 py-4 bg-white/90 shadow-md overflow-hidden sm:rounded-lg">
+                <div className=" w-full max-w-md px-6 py-4 bg-white/90 shadow-md overflow-hidden sm:rounded-lg">
                     {children}
                 </div>
             </div>
